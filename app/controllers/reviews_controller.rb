84cc014abf9335e392943c1906d2e195139956e8
Aymard de Dieuleveult <aymard.de.dieu@gmail.com>
2023-11-22 16:57:37 +0100

    on va samuser

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,14 +9,6 @@ class ReviewsController < ApplicationController
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
-    # respond_to do |format|
-    #   if @review.save
-    #     format.html { redirect_to restaurant_url(@restaurant), notice: "Votre avis a bien été pris en compte !" }
-    #     format.json { render :show, status: :created, location: @restaurant }
-    #   else
-    #     format.html { render :new, status: :unprocessable_entity }
-    #     format.json { render json: @restaurants.errors, status: :unprocessable_entity }
-    #   end
     if @review.save
       redirect_to restaurant_path(@restaurant)
     else
